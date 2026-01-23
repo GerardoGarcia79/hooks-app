f4570c427cb80f86de0f0efbe477115eb6123117
Gerardo de Leon Garcia <gerardodlg.dev@gmail.com>
2026-01-23 12:42:15 -0600

    feat: object validation using zod

--- src/05-useReducer/reducer/tasksReducer.ts
+++ src/05-useReducer/reducer/tasksReducer.ts
@@ -1,3 +1,5 @@
+import * as z from "zod/v4";
+
 interface Todo {
   id: number;
   text: string;
@@ -16,11 +18,36 @@ export type TaskAction =
   | { type: "TOGGLE_TODO"; payload: number }
   | { type: "DELETE_TODO"; payload: number };
 
+const TodoSchema = z.object({
+  id: z.number(),
+  text: z.string(),
+  completed: z.boolean(),
+});
+
+const TaskStateSchema = z.object({
+  todos: z.array(TodoSchema),
+  length: z.number(),
+  completed: z.number(),
+  pending: z.number(),
+});
+
 export const getTasksInitialState = (): TaskState => {
   const todosFromLocalStorage = localStorage.getItem("todos");
   if (todosFromLocalStorage) {
-    // ! Cuidado, el objeto puede haber sido manipulado
-    return JSON.parse(todosFromLocalStorage);
+    const result = TaskStateSchema.safeParse(JSON.parse(todosFromLocalStorage));
+
+    if (result.error) {
+      // console.log(result.error);
+
+      return {
+        todos: [],
+        completed: 0,
+        pending: 0,
+        length: 0,
+      };
+    }
+
+    return result.data;
   }
 
   return {
