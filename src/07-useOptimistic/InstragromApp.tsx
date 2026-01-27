a3001526645bf07fb4e64756c9de0e73680e61ad
Gerardo de Leon Garcia <gerardodlg.dev@gmail.com>
2026-01-27 16:27:12 -0600

    feat: simulate fail in useOptimistic + sonner (toast library)

--- src/07-useOptimistic/InstragromApp.tsx
+++ src/07-useOptimistic/InstragromApp.tsx
@@ -1,4 +1,5 @@
 import { useOptimistic, useState, useTransition } from "react";
+import { toast } from "sonner";
 
 interface Comment {
   id: number;
@@ -38,10 +39,23 @@ export const InstagromApp = () => {
       // Simular petición al servidor
       await new Promise((resolve) => setTimeout(resolve, 3000));
 
-      setComments((prev) => [
-        ...prev,
-        { id: new Date().getTime(), text: messageText },
-      ]);
+      // setComments((prev) => [
+      //   ...prev,
+      //   { id: new Date().getTime(), text: messageText },
+      // ]);
+
+      // ! Código para revertir el proceso si falla
+      // ! También podemos guardar desde antes el id, y eliminar el item
+      setComments((prev) => prev);
+      toast("Error al agregar el comentario", {
+        description: "Intente nuevamente",
+        duration: 10_000,
+        position: "top-right",
+        action: {
+          label: "Cerrar",
+          onClick: () => toast.dismiss(),
+        },
+      });
     });
   };
 
