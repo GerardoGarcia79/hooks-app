ab9eb1fa0c9c3ccfdf5818ca85264e52d8f719d3
Gerardo de Leon Garcia <gerardodlg.dev@gmail.com>
2026-01-30 12:29:06 -0600

    feat: user context - global information

--- src/09-useContext/context/UserContext.tsx
+++ src/09-useContext/context/UserContext.tsx
@@ -1,9 +1,55 @@
-import { type PropsWithChildren } from "react";
+import { createContext, useState, type PropsWithChildren } from "react";
+import { users, type User } from "../data/user-mock-data";
 
 // interface Props {
 //   children: React.ReactNode;
 // }
 
+type AuthStatus = "checking" | "authenticated" | "not-authenticated";
+
+interface UserContextProps {
+  // state
+  authStatus: AuthStatus;
+  user: User | null;
+
+  // methods
+  login: (userId: number) => boolean;
+  logout: () => void;
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const UserContext = createContext({} as UserContextProps);
+
 export const UserContextProvider = ({ children }: PropsWithChildren) => {
-  return <>{children}</>;
+  const [authStatus, setAuthStatus] = useState<AuthStatus>("checking");
+  const [user, setUser] = useState<User | null>(null);
+
+  const handleLogin = (userId: number): boolean => {
+    const user = users.find((user) => user.id === userId);
+    setAuthStatus("authenticated");
+    if (!user) {
+      setUser(null);
+    } else {
+      setUser(user);
+    }
+    return true;
+  };
+
+  const handleLogout = () => {
+    setAuthStatus("not-authenticated");
+    setUser(null);
+  };
+
+  return (
+    <UserContext
+      value={{
+        authStatus,
+        user,
+        login: handleLogin,
+        logout: handleLogout,
+      }}
+    >
+      {children}
+    </UserContext>
+  );
 };
